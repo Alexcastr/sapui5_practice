c95a608a22c8511483d8a03995d5396753186a4e
alexcastr <acastro@logaligroup.com>
2025-07-07 16:18:03 -0500

    Include an Input field for dynamic data binding

--- 02-project-ts/webapp/controller/App.controller.ts
+++ 02-project-ts/webapp/controller/App.controller.ts
@@ -1,11 +1,22 @@
 import Controller from "sap/ui/core/mvc/Controller";
-
+import MessageToast from "sap/m/MessageToast";
+import JSONModel from "sap/ui/model/json/JSONModel";
 /**
  * @name ui5.02-project-ts.controller.App
  */
 export default class AppController extends Controller {
+
+    onInit() : void {
+      // set data model on view
+        const data = {
+           recipient: {
+              name: "It is a pleasure to meet you!"
+           }
+        };
+        const dataModel = new JSONModel(data);
+        this.getView()?.setModel(dataModel);
+    }
     onShowHello(): void {
-        // show a native JavaScript alert
-        alert("Hello World");
+        MessageToast.show("Hello World");
      }
 };
