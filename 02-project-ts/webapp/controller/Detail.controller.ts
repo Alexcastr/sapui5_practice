ef67abda7df5addebf460bf59b756f8bb5974ccb
alexcastr <acastro@logaligroup.com>
2025-07-23 16:17:27 -0500

    32 routing back and history

--- 02-project-ts/webapp/controller/Detail.controller.ts
+++ 02-project-ts/webapp/controller/Detail.controller.ts
@@ -1,6 +1,8 @@
-import Controller from "sap/ui/core/mvc/Controller";
-import UIComponent from "sap/ui/core/UIComponent";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
+
+import UIComponent from "sap/ui/core/UIComponent";
+import Controller from "sap/ui/core/mvc/Controller";
+import History from "sap/ui/core/routing/History";
 
 export default class Detail extends Controller {
 
@@ -34,6 +36,20 @@ export default class Detail extends Controller {
                 path: "/" + decodedPath,
                 model: "invoice"
             });
+        }
+    }
+
+    onNavBack(): void {
+        const history = History.getInstance();
+
+        const previousHash = history.getPreviousHash();
+
+        if (previousHash !== undefined) {
+            console.log("Previous Hash:", previousHash);
+            window.history.go(-1);
+        } else {
+            const router = UIComponent.getRouterFor(this);
+            router.navTo("overview", {}, true);
         }
     }
 };
