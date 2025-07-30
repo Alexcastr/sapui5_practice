3bedafe95a6f5799ebcb681733ca1f76a48dd7ab
alexcastr <acastro@logaligroup.com>
2025-07-30 16:51:18 -0500

    35 device adaptation

--- 02-project-ts/webapp/Component.ts
+++ 02-project-ts/webapp/Component.ts
@@ -1,9 +1,9 @@
-import ResourceModel from "sap/ui/model/resource/ResourceModel";
+
 import UIComponent from "sap/ui/core/UIComponent";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import XMLView from "sap/ui/core/mvc/XMLView";
 import Control from "sap/ui/core/Control";
-
+import Device from "sap/ui/Device";
 /**
 * @namespace 02-project-ts
 */
@@ -24,6 +24,12 @@ export default class Component extends UIComponent {
         };
         const dataModel = new JSONModel(data);
         this.setModel(dataModel);
+
+         // set device model
+        const deviceModel = new JSONModel(Device);
+        deviceModel.setDefaultBindingMode("OneWay");
+        this.setModel(deviceModel, "device");
+
         
         this.getRouter().initialize();
     };
